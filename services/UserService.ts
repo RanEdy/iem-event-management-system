90d2faf4647b4f5757ab1276bd7d29a36a2b07e8
Angel <villagomez.jose@uabc.edu.mx>
2025-04-26 20:21:22 -0700

    UserServices documentado.
    Agregue la documentacion de la clase UserServices.

--- services/UserService.ts
+++ services/UserService.ts
@@ -1,20 +1,40 @@
 import { IUser } from "@/entities/IUser";
 import { DAOLocator } from "@/persistence/DAOLocator";
 
+
+/**
+ * Class with generic methods for CRUD.
+ * Utilizes Prisma ORM for database communication through DAO instances.
+ * @author Erandi Angel
+ */
 export class UserService
 {
     constructor() {}
 
+    /**
+     * Searches for a unique entry that matches with the id in the user table.
+     * @param id The primary key of the user.
+     * @returns A unique object of type User if found. Otherwise return null.
+     */
     async findById(id: number): Promise<IUser | null>
     {
         return DAOLocator.userDao.findById(id);
     }
 
+    /**
+     * Searches for all the entries of the user table.
+     * @returns An array of user objects found in the table.
+     */
     async findAll(): Promise<IUser[]>
     {
         return DAOLocator.userDao.findAll();
     }
 
+    /**
+    * Creates a new user entry in the database.
+    * @param userData The object containing the user's information, excluding the ID.
+    * @returns A boolean, "true" if the creation was successful, otherwise "false".
+    */
     async create(userData: Omit<IUser, 'id'>): Promise<boolean>
     {
         try
@@ -29,6 +49,11 @@ export class UserService
         }
     }
 
+    /**
+    * Updates an existing user entry in the database.
+    * @param userData The object containing the user's updated information.
+    * @returns A boolean, "true" if the update was successful, otherwise "false".
+    */
     async update(userData: IUser): Promise<boolean>
     {
         try
@@ -43,6 +68,11 @@ export class UserService
         }
     }
 
+    /**
+    * Inserts or updates a user entry in the database.
+    * @param userData The object containing the user's information.
+    * @returns A boolean, "true" if the operation was successful, otherwise "false".
+    */
     async upsert(userData: IUser): Promise<boolean>
     {
         try
@@ -57,6 +87,11 @@ export class UserService
         }
     }
 
+    /**
+    * Deletes a user entry from the database by its ID.
+    * @param id The primary key of the user to be deleted.
+    * @returns A boolean, "true" if the deletion was successful, otherwise "false".
+    */
     async deleteById(id: number): Promise<boolean>
     {
         try

0af8473c16f68d2ffd415ca0db0e5a1465f794a9
Erandi Sacbe <erandi.moreno@uabc.edu.mx>
2025-04-28 11:39:26 -0700

    Eliminación de importaciones innecesarias

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,6 +1,4 @@
 import { EventsPage } from "@/components/eventsUI/EventsPage"
-import { EventsTable } from "@/components/eventsUI/EventsTable"
-import DataTable from "react-data-table-component"
 
 export default function MainPage()
 {

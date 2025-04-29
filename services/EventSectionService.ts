88e6f0150ac376ec57e59af1802b5260138e4ce2
Erandi Sacbe <erandi.moreno@uabc.edu.mx>
2025-04-29 16:36:00 -0700

    [PBI-EVNT-US6] Codificar las clases de servicios de la lógica de negocios para las Secciones y Archivos

---
+++ services/EventSectionService.ts
@@ -0,0 +1,101 @@
+import { IEventSection } from "@/entities/IEventSection";
+import { DAOLocator } from "@/persistence/DAOLocator";
+
+export class EventSectionService {
+    constructor() {}
+    
+        /**
+         * Searches for a unique entry that matches with the id in the eventSection table.
+         * @param id The primary key of the eventSection.
+         * @returns A unique object of type eventSection if found. Otherwise return null.
+         */
+        async findById(id: number): Promise<IEventSection | null>
+        {
+            return DAOLocator.eventSectionDao.findById(id);
+        }
+    
+        /**
+         * Searches for all the entries of the eventSection table.
+         * @returns An array of eventSection objects found in the table.
+         */
+        async findAll(): Promise<IEventSection[]>
+        {
+            return DAOLocator.eventSectionDao.findAll();
+        }
+    
+        /**
+        * Creates a new eventSection entry in the database.
+        * @param eventSectionData The object containing the eventSection's information, excluding the ID.
+        * @returns A boolean, "true" if the creation was successful, otherwise "false".
+        */
+        async create(eventSectionData: Omit<IEventSection, 'id'>): Promise<boolean>
+        {
+            try
+            {
+                await DAOLocator.eventSectionDao.create(eventSectionData);
+                return true;
+            }
+            catch(error)
+            {
+                console.error("Event Section could not be created");
+                return false;
+            }
+        }
+    
+        /**
+        * Updates an existing eventSection entry in the database.
+        * @param eventSectionData The object containing the eventSection's updated information.
+        * @returns A boolean, "true" if the update was successful, otherwise "false".
+        */
+        async update(eventSectionData: IEventSection): Promise<boolean>
+        {
+            try
+            {
+                await DAOLocator.eventSectionDao.update(eventSectionData);
+                return true;
+            }
+            catch(error)
+            {
+                console.error("Event Section could not be updated");
+                return false;
+            }
+        }
+    
+        /**
+        * Inserts or updates a eventSection entry in the database.
+        * @param eventSectionData The object containing the eventSection's information.
+        * @returns A boolean, "true" if the operation was successful, otherwise "false".
+        */
+        async upsert(eventSectionData: IEventSection): Promise<boolean>
+        {
+            try
+            {
+                await DAOLocator.eventSectionDao.upsert(eventSectionData);
+                return true;
+            }
+            catch(error)
+            {
+                console.error("Event Section could not be updated or created (upsert)");
+                return false;
+            }
+        }
+    
+        /**
+        * Deletes a eventSection entry from the database by its ID.
+        * @param id The primary key of the eventSection to be deleted.
+        * @returns A boolean, "true" if the deletion was successful, otherwise "false".
+        */
+        async deleteById(id: number): Promise<boolean>
+        {
+            try
+            {
+                await DAOLocator.eventSectionDao.deleteById(id);
+                return true;
+            }
+            catch(error)
+            {
+                console.error("Event Section could not be deleted");
+                return false;
+            }
+        }
+}
---
+++ services/SectionFileService.ts
@@ -0,0 +1,101 @@
+import { ISectionFile } from "@/entities/ISectionFile";
+import { DAOLocator } from "@/persistence/DAOLocator";
+
+export class SectionFileService {
+    constructor() {}
+    
+        /**
+         * Searches for a unique entry that matches with the id in the sectionFile table.
+         * @param id The primary key of the sectionFile.
+         * @returns A unique object of type sectionFile if found. Otherwise return null.
+         */
+        async findById(id: number): Promise<ISectionFile | null>
+        {
+            return DAOLocator.sectionFileDao.findById(id);
+        }
+    
+        /**
+         * Searches for all the entries of the sectionFileData table.
+         * @returns An array of sectionFileData objects found in the table.
+         */
+        async findAll(): Promise<ISectionFile[]>
+        {
+            return DAOLocator.sectionFileDao.findAll();
+        }
+    
+        /**
+        * Creates a new sectionFileData entry in the database.
+        * @param sectionFileData The object containing the sectionFileData's information, excluding the ID.
+        * @returns A boolean, "true" if the creation was successful, otherwise "false".
+        */
+        async create(sectionFileData: Omit<ISectionFile, 'id'>): Promise<boolean>
+        {
+            try
+            {
+                await DAOLocator.sectionFileDao.create(sectionFileData);
+                return true;
+            }
+            catch(error)
+            {
+                console.error("Section File could not be created");
+                return false;
+            }
+        }
+    
+        /**
+        * Updates an existing sectionFileData entry in the database.
+        * @param sectionFileData The object containing the sectionFileData's updated information.
+        * @returns A boolean, "true" if the update was successful, otherwise "false".
+        */
+        async update(sectionFileData: ISectionFile): Promise<boolean>
+        {
+            try
+            {
+                await DAOLocator.sectionFileDao.update(sectionFileData);
+                return true;
+            }
+            catch(error)
+            {
+                console.error("Section File could not be updated");
+                return false;
+            }
+        }
+    
+        /**
+        * Inserts or updates a sectionFileData entry in the database.
+        * @param eventSectionData The object containing the sectionFileData's information.
+        * @returns A boolean, "true" if the operation was successful, otherwise "false".
+        */
+        async upsert(sectionFileData: ISectionFile): Promise<boolean>
+        {
+            try
+            {
+                await DAOLocator.sectionFileDao.upsert(sectionFileData);
+                return true;
+            }
+            catch(error)
+            {
+                console.error("Section File could not be updated or created (upsert)");
+                return false;
+            }
+        }
+    
+        /**
+        * Deletes a sectionFileData entry from the database by its ID.
+        * @param id The primary key of the sectionFileData to be deleted.
+        * @returns A boolean, "true" if the deletion was successful, otherwise "false".
+        */
+        async deleteById(id: number): Promise<boolean>
+        {
+            try
+            {
+                await DAOLocator.sectionFileDao.deleteById(id);
+                return true;
+            }
+            catch(error)
+            {
+                console.error("Section File could not be deleted");
+                return false;
+            }
+        }
+}

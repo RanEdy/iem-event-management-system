706fc02d272bba633472d92b13846b8855328eae
enrique Gudiño <137984675+Catrin101@users.noreply.github.com>
2025-05-06 15:24:44 -0700

    [PBI-EVNT-US4]
    Se corrigió el script de ArchivesTable.tsx para que coincida con las modificaciones echas en la base de datos

--- components/ArchivesUI/ArchivesTable.tsx
+++ components/ArchivesUI/ArchivesTable.tsx
@@ -21,7 +21,7 @@ const columns: TableColumn<IEvent>[] = [
                     + ", " + row.city + " " + row.zipCode + ". "}
                 </div>
                 <div className="font-extrabold text-zinc-600">
-                    {row.externalNumber + " " + row.street + " #" + row.internalNumber}
+                    {row.address}
                 </div>
             </div>
         </div>,
@@ -81,7 +81,7 @@ export const ArchivesTable: React.FC = () =>
         .then((data: IEvent[]) => {
             // Convert date strings to Date objects and filter only completed events
             const parsedEvents = data
-                .filter(event => event.done === true)
+                .filter(event => event.status === true)
                 .map(event => ({
                     ...event,
                     startDate: new Date(event.startDate),
